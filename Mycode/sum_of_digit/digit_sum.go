3dc061b7151ceab82811504154f3f9872cb7b5a7
Hemant <hmukati061094@gmail.com>
2021-11-12 19:34:17 +0530

    code for sum of digits of number

---
+++ Mycode/sum_of_digit/digit_sum.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main(){
+      var num = 989
+	  x:= 10
+	  
+	 a := num/x
+	 b := a/x
+     c := b/x
+    fmt.Println(c)
+	     j:=num%x
+         k:=a%x
+         l:=b%x
+      sum:=(j+k+l)
+ 	fmt.Println(sum)	 
+ }
